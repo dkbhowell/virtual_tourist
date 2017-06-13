9485423fc9aa4d3b0394dbd86785a667afc73a58
Dustin Howell <dkbhowell@gmail.com>
2017-06-12 17:17:42 -0700

    added app icon and renamed initial VC

--- VirtualTourist/LocationsMapViewController.swift
+++ VirtualTourist/LocationsMapViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LocationsMapViewController.swift
 //  VirtualTourist
 //
 //  Created by Dustin Howell on 6/12/17.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LocationsMapViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
